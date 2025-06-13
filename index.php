a2d2824c92e40665c3b974fc7b7f8a3f0f5cd1c3
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-06-13 15:07:28 +0700

    footer

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <script src="./assets/script/script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./assets/style/style.css">
     <title>Bappeda - Perpustakaan</title>
 </head>
@@ -82,17 +83,67 @@
     Possimus laborum dolore rerum facilis cum suscipit quibusdam. Doloribus magnam incidunt cum, expedita numquam eius autem odit neque eligendi. Sapiente odit tempore libero sed deleniti natus reprehenderit doloribus! Itaque, numquam!
     Nesciunt, consectetur numquam et tenetur, minima laudantium alias ex pariatur nemo nam saepe aut, quia eius illum neque! Nisi sunt officia totam voluptatem, eaque aspernatur molestiae eligendi. Eligendi, quia inventore?</h3>
     <footer>
-        <div class="container-fluid p-5 footer-color">
-            <div>
-                <div class="row">
-                    <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestiae officia quis ex incidunt rerum nostrum, dignissimos cumque expedita iste sed neque iure commodi vitae beatae eligendi? Tempora, nihil dicta!</div>
-                    <div class="col">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum ipsam ullam aliquam corrupti alias amet magni laudantium voluptatum veritatis quibusdam eveniet a repudiandae asperiores expedita rerum, repellat distinctio tempore. Saepe?</div>
-                    <div class="col">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis accusamus doloribus soluta fuga eveniet amet blanditiis reiciendis! At exercitationem odit ea nemo laboriosam, provident deserunt, dolor porro corporis quaerat voluptatem.</div>
-                    <div class="col">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptates illum officiis magnam. Facilis enim aliquid quas commodi, pariatur libero blanditiis alias repellendus esse non accusantium vel doloremque nostrum nobis.</div>
+    <div class="container-fluid p-5 footer-color">
+        <div>
+            <div class="row">
+                <div class="col-6 footer-content">
+                    <div class="logo-section">
+                        <img src="./assets/img/Lampung_coa.png" alt="Logo" class="footer-logo">
+                    </div>
+                    <div class="footer-text">
+                        <p>Bappeda Provinsi Lampung merupakan lembaga perencana pembangunan daerah yang membantu Gubernur dalam perumusan kebijakan pembangunan. Sejak dibentuk tahun 1980, struktur dan tugasnya telah beberapa kali mengalami penyesuaian sesuai peraturan perundang-undangan, terakhir melalui Pergub No. 88 Tahun 2016.</p>
+                    </div>
+                    <div class="social-icons">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="mailto:example@example.com"><i class="fas fa-envelope"></i></a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="row contact-info">
+                        <div class="col-12 d-flex align-items-center mb-3">
+                            <i class="fas fa-phone-alt contact-icon"></i>
+                            <div class="contact-details">
+                                <span class="contact-label">Kontak</span>
+                                <span class="contact-value">(0721) 485458</span>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row contact-info">
+                        <div class="col-12 d-flex align-items-center mb-3">
+                            <i class="fas fa-envelope contact-icon"></i>
+                            <div class="contact-details">
+                                <span class="contact-label">E-Mail</span>
+                                <span class="contact-value">example@gmail.com</span>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row contact-info">
+                        <div class="col-12 d-flex align-items-center mb-3">
+                            <i class="fas fa-clock contact-icon"></i>
+                            <div class="contact-details">
+                                <span class="contact-label">Jam Kerja</span>
+                                <span class="contact-value">Monday - Fri: 7.30 am - 4.00 pm</span>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row contact-info">
+                        <div class="col-12 d-flex align-items-center mb-3">
+                            <i class="fas fa-map-marker-alt contact-icon"></i>
+                            <div class="contact-details">
+                                <span class="contact-label">Lokasi</span>
+                                <span class="contact-value">Jalan Robert Wolter Monginsidi No. 223</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
