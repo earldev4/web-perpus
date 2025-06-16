db3b5b37f71e360febeefe5754f18b12950e783c
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-06-16 13:18:23 +0700

    atomization

---
+++ components/stats.php
@@ -0,0 +1,43 @@
+<!-- components/stats.php -->
+<div class="stats-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-6">
+                    <div class="stat-card">
+                        <div class="stat-icon">
+                            <i class="fas fa-book"></i>
+                        </div>
+                        <div class="stat-number">1,250+</div>
+                        <div class="stat-label">Total Buku</div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="stat-card">
+                        <div class="stat-icon">
+                            <i class="fas fa-users"></i>
+                        </div>
+                        <div class="stat-number">350+</div>
+                        <div class="stat-label">Anggota Aktif</div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="stat-card">
+                        <div class="stat-icon">
+                            <i class="fas fa-bookmark"></i>
+                        </div>
+                        <div class="stat-number">25</div>
+                        <div class="stat-label">Kategori</div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-6">
+                    <div class="stat-card">
+                        <div class="stat-icon">
+                            <i class="fas fa-clock"></i>
+                        </div>
+                        <div class="stat-number">24/7</div>
+                        <div class="stat-label">Akses Online</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
