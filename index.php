7a2d2dceb01615f57b44451313741ca391d3464d
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-06-16 13:19:38 +0700

    Update index.php untuk integrasi

--- index.php
+++ index.php
@@ -7,145 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./assets/style/style.css">
-    <script src="./assets/script/script.js"></script>
+    <link rel="stylesheet" href="./assets/style/carousel.css">
+    <script src="./assets/script/script.js"></script>   
     <title>Bappeda - Perpustakaan</title>
 </head>
 <body>
     <?php include 'components/navbar.php'; ?>
-    <h1 class="text-center my-3 py-1">Selamat Datang Di Website Perpustakaan <br>Bappeda Lampung</h1>
-    <div id="carouselExampleIndicators" class="carousel slide mx-auto" style="max-width: 1200px;" data-bs-ride="carousel" data-bs-touch="true">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row justify-content-center gx-3 gy-3">
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku1.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Pemerintahan</p>
-                                    <h6 class="fw-bold mb-1">Buku 1</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku2.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Politik</p>
-                                    <h6 class="fw-bold mb-1">Buku2</h6>
-                                    <p class="text-muted small mb-1">Michael</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku3.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Hewan</p>
-                                    <h6 class="fw-bold mb-1">Buku3</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku3.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Hewan</p>
-                                    <h6 class="fw-bold mb-1">Buku3</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item ">
-                <div class="container">
-                    <div class="row justify-content-center gx-2 gy-3">
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku4.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Teknologi</p>
-                                    <h6 class="fw-bold mb-1">Buku 4</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku5.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Sosial</p>
-                                    <h6 class="fw-bold mb-1">Buku 5</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku1.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Politik</p>
-                                    <h6 class="fw-bold mb-1">Buku6</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                            <div class="card">
-                                <img src="./assets/img/buku/buku1.jpg" class="card-img-top" alt="Book 1" style="height: 220px; object-fit: cover;">
-                                <div class="card-body text-center">
-                                    <p class="text-muted small mb-0">Politik</p>
-                                    <h6 class="fw-bold mb-1">Buku6</h6>
-                                    <p class="text-muted small mb-1">Alexander</p>
-                                    <a href="#" class="btn btn-outline-primary btn-sm">Pinjam Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit minima illo nemo hic earum laborum eius fugiat, quasi optio magnam minus? Consectetur eos consequatur dolorem! Tenetur voluptatum illo aut eum.
-    Quam, magnam. Officia atque voluptatum dolorum quos eaque quis veniam amet iusto eum consequuntur odio natus nemo, dicta repudiandae repellat quidem quia sed numquam, ut, distinctio reprehenderit sequi sit reiciendis.
-    In temporibus cum, tempora fuga dolor tempore nihil maiores sequi eveniet tenetur placeat sit nam cupiditate quod neque magni! Veniam accusantium quos id, quam ea velit sed excepturi totam modi.
-    Nihil laboriosam, iure mollitia rerum corrupti ut tenetur consequuntur velit animi id, maiores enim. Praesentium, distinctio aut facilis minima molestiae, veniam omnis quas cumque iure rerum at alias error reprehenderit!
-    Quia, laborum? Facilis consequatur autem voluptatibus excepturi voluptate, porro quae odit cumque quidem nesciunt id. Facilis commodi nisi doloribus impedit corrupti. Unde ducimus veniam rerum minus enim mollitia sint dolor.
-    Quis sunt sit similique blanditiis impedit sequi optio. Amet nam voluptatibus aliquid, consectetur, sit ipsam excepturi at possimus consequatur itaque optio provident veritatis laborum eum quos eos aut unde eveniet?
-    Iste debitis architecto fugit voluptate tempora suscipit veniam omnis neque cum. Saepe natus porro dolores sit corrupti a nobis cumque enim sequi ex? Nobis odit adipisci non, incidunt blanditiis at.
-    Necessitatibus consequuntur suscipit porro dolores cum asperiores, laboriosam eaque illo voluptates recusandae nam obcaecati vero officiis fugit corporis unde quibusdam nesciunt nostrum odit beatae quam. Veniam laudantium pariatur ad libero!
-    Quas odio expedita minima tenetur et? Quisquam odio, dolorum quibusdam magni magnam delectus, accusamus doloremque nesciunt iste dolore voluptates, fuga id error nulla neque. Officiis dolor distinctio animi error neque.
-    Ratione distinctio autem, accusamus magni aspernatur, laborum nemo debitis nam error corporis magnam rem dicta cum aut consequatur eveniet adipisci similique deleniti nulla a possimus tenetur ducimus? Doloribus, repudiandae soluta.
-    Sint obcaecati rerum modi numquam incidunt deserunt ex culpa tempora, ipsam magni nobis mollitia quas ullam eum! Provident quo eaque, numquam neque amet dolor, quisquam voluptatum omnis veniam consectetur quia?
-    Totam inventore, sit quas autem praesentium dolores magnam alias minima pariatur reiciendis maiores nulla non cum odio incidunt tempore? Amet reiciendis cupiditate aliquam deserunt eaque beatae aliquid, quae vitae cumque.
-    Sit, consequatur sed incidunt voluptas nemo libero voluptatum nesciunt eveniet cum doloribus aliquid repellat! Corporis quo sapiente esse reprehenderit, fugiat quisquam, animi cupiditate et sint necessitatibus asperiores. Tempore, nostrum suscipit!
-    Vitae facere voluptate omnis tempore obcaecati molestias magnam necessitatibus perspiciatis et? Natus optio consequatur quaerat quia, necessitatibus ad quo asperiores accusamus velit, expedita hic voluptatem? Consequatur natus dolore sit. Perferendis!
-    Distinctio neque ea in tenetur sequi! Vitae sit qui minus quasi perferendis dolorum porro labore blanditiis deleniti suscipit quis quod, id quisquam sint accusamus aliquam ex eius minima a beatae.
-    Sequi, qui earum dolorum esse explicabo inventore perferendis, harum odio atque nobis dolore, omnis dolorem beatae corrupti quae dolor neque enim? Dolorum nulla, cumque nostrum corporis tempora ratione enim error.
-    Temporibus odio nobis aperiam! Aliquid, eligendi in repellat, veritatis debitis ipsam porro amet quis consequatur pariatur ipsum expedita quos quasi autem voluptate. Numquam, molestiae voluptates. Earum quibusdam aspernatur incidunt delectus.
-    Enim, tempora officiis. Cum amet debitis quis nam vero? Quis nisi dolores quas quae. Architecto placeat necessitatibus veritatis ullam, totam, reiciendis consectetur autem doloremque, possimus eius consequuntur odio quasi molestias.
-    Possimus laborum dolore rerum facilis cum suscipit quibusdam. Doloribus magnam incidunt cum, expedita numquam eius autem odit neque eligendi. Sapiente odit tempore libero sed deleniti natus reprehenderit doloribus! Itaque, numquam!
-    Nesciunt, consectetur numquam et tenetur, minima laudantium alias ex pariatur nemo nam saepe aut, quia eius illum neque! Nisi sunt officia totam voluptatem, eaque aspernatur molestiae eligendi. Eligendi, quia inventore?</h3>
     
+    <div class="hero-section">
+        <div class="container">
+            <h1 class="hero-title">Selamat Datang Di Website Perpustakaan <br>Bappeda Lampung</h1>
+            <p class="hero-subtitle">Temukan koleksi buku terbaik untuk menambah wawasan dan pengetahuan Anda</p>
+        </div>
+    </div>
+
+    <?php include 'components/carousel.php'; ?>
+
+    <?php include 'components/stats.php'; ?>
+
     <?php include 'components/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
