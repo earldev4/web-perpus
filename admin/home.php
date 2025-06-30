db4fc3ba3a3fe1df51793cdb93306497e1b5d87d
Deva Ahmad <devaahmad43@gmail.com>
2025-06-30 11:04:20 +0700

    separating side bar to another folder

--- admin/home.php
+++ admin/home.php
@@ -61,25 +61,7 @@ if (isset($_SESSION["is_login"]) == false) {
     <div class="container-fluid">
         <div class="row min-vh-100">
             <div class="col-md-2 col-12 bg-primary">
-                <nav>
-                    <div class="d-flex flex-column align-items-center py-3 px-2">
-                        <i class="fa-solid fa-circle-user img-profile" style="font-size: 80px;"></i>
-                        <h3 class="text-center p-2">Selamat datang <?= isset($_SESSION['nama_user']) ? $_SESSION['nama_user'] : "Belom login" ?></h3>
-                    </div>
-                    <div class="kumpulanLink">
-                        <div class="page-home"><a href="" class="text-light">Home</a></div>
-                        <div class="page-profile"><a href="" class="text-light">Profile</a></div>
-                        <div class="page-profile"><a href="" class="text-light">Catalog</a></div>
-                        <div class="page-socialmedia"><a href="" class="text-light">Social Media</a></div>
-                        <div class="page-backweb"><a href="../index.php" class="text-light">Back To Web</a></div>
-                        <div class="page-logout p-3">
-                            <form action=<?= $navlogout ?> method="POST" id="form_logout" name="form_logout">
-                                <input type="hidden" name="logout" class="btn btn-danger w-100" value="">
-                                <button type="submit" class="btn btn-danger w-100">Logout</button>
-                            </form>
-                        </div>
-                    </div>
-                </nav>
+                <?php include 'pages/nav.php' ?>
             </div>
             <div class="col-md-10 col-12 bg-success">
                 <div>
