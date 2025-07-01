bde5df98dfab531c5f6db89c25c26deaf926e4f7
Deva Ahmad <devaahmad43@gmail.com>
2025-07-01 11:51:34 +0700

    making page and routing 3

--- admin/pages/add_book.php
+++ admin/pages/add_book.php
@@ -1,65 +1,80 @@
 <?php
-require_once "../config/db.php";
+require_once __DIR__ . "/../../config/db.php";
+require_once __DIR__ . "/../../config/class.php";
+require_once __DIR__ . "/../config/class.php";
 
 $conn = getConnection();
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
-        if($_POST["judul_buku"] && $_FILES["gambar_buku"] && $_POST["kategori_buku"] && $_POST["penerbit_buku"] && $_POST["jumlah_buku"] && $_POST["jumlah_halaman"] && $_POST["deskripsi_buku"] && $_POST["bahasa_buku"] && $_POST["isbn_buku"]){
-            $judul_buku = $_POST["judul_buku"];
-            $gambar_buku = $_FILES["gambar_buku"];
-            $gambar_buku_name = $_FILES["gambar_buku"]["name"];
-            $gambar_buku_temp = $_FILES["gambar_buku"]["tmp_name"];
-            $gambar_buku_size = $_FILES["gambar_buku"]["size"];
-            $gambar_buku_type = $_FILES["gambar_buku"]["type"];
-            $kategori_buku = $_POST["kategori_buku"];
-            $penerbit_buku = $_POST["penerbit_buku"];
-            $jumlah_buku = $_POST["jumlah_buku"];
-            $jumlah_halaman = $_POST["jumlah_halaman"];
-            $deskripsi_buku = $_POST["deskripsi_buku"];
-            $bahasa_buku = $_POST["bahasa_buku"];
-            $isbn_buku = $_POST["isbn_buku"];
-            $file_ext = explode('.', $gambar_buku_name);
-            $file_ext_actual = strtolower(end($file_ext));
-            $allowed = array('jpg', 'jpeg', 'png');
+$perpustakaan = new Perpustakaan($conn);
 
-            if($judul_buku && $gambar_buku && $kategori_buku && $penerbit_buku && $jumlah_buku && $jumlah_halaman && $deskripsi_buku && $bahasa_buku && $isbn_buku){
-                if (in_array($file_ext_actual, $allowed)) {
-                    if($gambar_buku_size < 5000000){
-                        $sql = <<<SQL
-                            INSERT INTO informasi (jumlah_halaman, bahasa_buku, isbn_buku) VALUES (?, ?, ?);
-                        SQL;
-                        $sql = $conn->prepare($sql);
-                        $sql->bindParam(1, $jumlah_halaman);
-                        $sql->bindParam(2, $bahasa_buku);
-                        $sql->bindParam(3, $isbn_buku);
-                        $sql->execute();
-                        $id_informasi = $conn->lastInsertId();
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if (isset($_POST['logout'])) {
+        $response = $perpustakaan->handleLogout("/../../pages/login.php");
+        echo json_encode($response);
+        exit();
+    }
+    if($_POST["judul_buku"] && $_FILES["gambar_buku"] && $_POST["kategori_buku"] && $_POST["penerbit_buku"] && $_POST["jumlah_buku"] && $_POST["jumlah_halaman"] && $_POST["deskripsi_buku"] && $_POST["bahasa_buku"] && $_POST["isbn_buku"]){
+        $judul_buku = $_POST["judul_buku"];
+        $gambar_buku = $_FILES["gambar_buku"];
+        $gambar_buku_name = $_FILES["gambar_buku"]["name"];
+        $gambar_buku_temp = $_FILES["gambar_buku"]["tmp_name"];
+        $gambar_buku_size = $_FILES["gambar_buku"]["size"];
+        $gambar_buku_type = $_FILES["gambar_buku"]["type"];
+        $kategori_buku = $_POST["kategori_buku"];
+        $penerbit_buku = $_POST["penerbit_buku"];
+        $jumlah_buku = $_POST["jumlah_buku"];
+        $jumlah_halaman = $_POST["jumlah_halaman"];
+        $deskripsi_buku = $_POST["deskripsi_buku"];
+        $bahasa_buku = $_POST["bahasa_buku"];
+        $isbn_buku = $_POST["isbn_buku"];
+        $file_ext = explode('.', $gambar_buku_name);
+        $file_ext_actual = strtolower(end($file_ext));
+        $allowed = array('jpg', 'jpeg', 'png');
 
-                        $gambar_name_new = uniqid('', true) . "." . $file_ext_actual;
-                        $gambar_folder = __DIR__ . '/../assets/img/buku/' . $gambar_name_new;
-                        move_uploaded_file($gambar_buku_temp, $gambar_folder);
-                        $sql = <<<SQL
-                            INSERT INTO buku (judul_buku, gambar_buku, kategori_buku, penerbit_buku, jumlah_buku, id_informasi, deskripsi_buku) VALUES (?, ?, ?, ?, ?, ?, ?);
-                        SQL;
-                        $sql = $conn->prepare($sql);
-                        $sql->bindParam(1, $judul_buku);
-                        $sql->bindParam(2, $gambar_name_new);
-                        $sql->bindParam(3, $kategori_buku);
-                        $sql->bindParam(4, $penerbit_buku);
-                        $sql->bindParam(5, $jumlah_buku);
-                        $sql->bindParam(6, $id_informasi);
-                        $sql->bindParam(7, $deskripsi_buku);
-                        $sql->execute();
+        if($judul_buku && $gambar_buku && $kategori_buku && $penerbit_buku && $jumlah_buku && $jumlah_halaman && $deskripsi_buku && $bahasa_buku && $isbn_buku){
+            if (in_array($file_ext_actual, $allowed)) {
+                if($gambar_buku_size < 5000000){
+                    $sql = <<<SQL
+                        INSERT INTO informasi (jumlah_halaman, bahasa_buku, isbn_buku) VALUES (?, ?, ?);
+                    SQL;
+                    $sql = $conn->prepare($sql);
+                    $sql->bindParam(1, $jumlah_halaman);
+                    $sql->bindParam(2, $bahasa_buku);
+                    $sql->bindParam(3, $isbn_buku);
+                    $sql->execute();
+                    $id_informasi = $conn->lastInsertId();
 
-                        echo "<script>alert('Buku berhasil ditambahkan!'); window.location.href='../pages/katalog.php';</script>";
-                        exit();
-                    } else {
-                        echo "<script>alert('Gambar buku terlalu besar! Maksimal 5MB');</script>";
-                        exit();
-                    }
+                    $gambar_name_new = uniqid('', true) . "." . $file_ext_actual;
+                    $gambar_folder = __DIR__ . '/../assets/img/buku/' . $gambar_name_new;
+                    move_uploaded_file($gambar_buku_temp, $gambar_folder);
+                    $sql = <<<SQL
+                        INSERT INTO buku (judul_buku, gambar_buku, kategori_buku, penerbit_buku, jumlah_buku, id_informasi, deskripsi_buku) VALUES (?, ?, ?, ?, ?, ?, ?);
+                    SQL;
+                    $sql = $conn->prepare($sql);
+                    $sql->bindParam(1, $judul_buku);
+                    $sql->bindParam(2, $gambar_name_new);
+                    $sql->bindParam(3, $kategori_buku);
+                    $sql->bindParam(4, $penerbit_buku);
+                    $sql->bindParam(5, $jumlah_buku);
+                    $sql->bindParam(6, $id_informasi);
+                    $sql->bindParam(7, $deskripsi_buku);
+                    $sql->execute();
+
+                    echo "<script>alert('Buku berhasil ditambahkan!'); window.location.href='../pages/katalog.php';</script>";
+                    exit();
+                } else {
+                    echo "<script>alert('Gambar buku terlalu besar! Maksimal 5MB');</script>";
+                    exit();
                 }
             }
         }
     }
+}
+$routing = new Routing("../home.php", "profile.php", "add_book.php", "social_media.php", "../../index.php", "add_book.php");
+
+if (isset($_SESSION["is_login"]) == false) {
+    header("location: ../../pages/login.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
@@ -75,40 +90,92 @@ $conn = getConnection();
     <title>Admin - Catalogue</title>
 </head>
 <body>
-    <form action="add_book.php" method="POST" enctype="multipart/form-data">
-        <h3>Data Buku</h3>
-        <label>Judul Buku:</label>
-        <input type="text" name="judul_buku" id="judul_buku" required><br>
-        
-        <label>Gambar Buku:</label>
-        <input type="file" name="gambar_buku" id="gambar_buku"><br>
-        
-        <label>Kategori Buku:</label>
-        <input type="text" name="kategori_buku" id="kategori_buku"><br>
-        
-        <label>Penerbit Buku:</label>
-        <input type="text" name="penerbit_buku" id="penerbit_buku"><br>
+    <div class="container-fluid">
+        <div class="row min-vh-100">
+            <div class="col-md-2 col-12 bg-primary">
+            <?php include 'nav.php'; ?>
+            </div>
+            <div class="col-md-10 col-12 bg-success">
+                <div>
+                    <h1>TAMBAH DATA BUKU</h1>
+                    <form action="add_book.php" method="POST" enctype="multipart/form-data">
+                        <h3>Data Buku</h3>
+                        <label class="form-label" for="judul_buku">Judul Buku:</label><br>
+                        <input class="form-control" type="text" name="judul_buku" id="judul_buku" required><br>
+                        
+                        <label class="form-label" for="gambar_buku">Gambar Buku:</label><br>
+                        <input class="form-control" type="file" name="gambar_buku" id="gambar_buku"><br>
+                        
+                        <label class="form-label" for="kategori_buku">Kategori Buku:</label><br>
+                        <input class="form-control" type="text" name="kategori_buku" id="kategori_buku"><br>
+                        
+                        <label class="form-label" for="penerbit_buku">Penerbit Buku:</label><br>
+                        <input class="form-control" type="text" name="penerbit_buku" id="penerbit_buku"><br>
 
-        <label>Jumlah Buku:</label>
-        <input type="number" name="jumlah_buku" id="jumlah_buku" required><br>
+                        <label class="form-label" for="jumlah_buku">Jumlah Buku:</label><br>
+                        <input class="form-control" type="number" name="jumlah_buku" id="jumlah_buku" required><br>
 
-        <label>Jumlah Halaman:</label>
-        <input type="number" name="jumlah_halaman" id="jumlah_halaman" required><br>
-        
-        <label>Bahasa Buku:</label>
-        <input type="text" name="bahasa_buku" id="bahasa_buku" required><br>
-        
-        <label>ISBN Buku:</label>
-        <input type="text" name="isbn_buku" id="isbn_buku" required><br>
+                        <label class="form-label" for="jumlah_halaman">Jumlah Halaman:</label><br>
+                        <input class="form-control" type="number" name="jumlah_halaman" id="jumlah_halaman" required><br>
+                        
+                        <label class="form-label" for="bahasa_buku">Bahasa Buku:</label><br>
+                        <input class="form-control" type="text" name="bahasa_buku" id="bahasa_buku" required><br>
+                        
+                        <label class="form-label" for="isbn_buku">ISBN Buku:</label><br>
+                        <input class="form-control" type="text" name="isbn_buku" id="isbn_buku" required><br>
 
-        <label>Deskripsi Buku:</label>
-        <input type="text" name="deskripsi_buku" id="deskripsi_buku" required><br>
-            
-        <button type="submit" name="submit">Simpan</button>
-    </form>
+                        <label class="form-label" for="deskripsi_buku">Deskripsi Buku:</label><br>
+                        <textarea name="deskripsi_buku" id="deskripsi_buku" class="form-control" rows="4" cols="50"></textarea><br>
+                            
+                        <button class="btn btn-primary w-100" type="submit" name="submit">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function(){
+            $('#form_logout').submit(function(e){
+                e.preventDefault();
+                let form = $(this);
+                let url = form.attr('action');
+                let method = form.attr('method');
+                let data = new FormData(form[0]);
+                console.log("Coba")
+                $.ajax({
+                    url: url,
+                    type: method,
+                    processData: false,
+                    contentType: false,
+                    data: data,
+                    dataType: 'JSON',
+                    success: function(response){
+                        if(response.status == "success"){
+                            toastr.success(response.message, "Success !",{
+                                closeButton: true,
+                                progressBar: true,
+                                timeOut: 1500
+                            });
+                            setTimeout(function(){
+                                if (response.redirect != "") {
+                                    location.href = response.redirect
+                                }
+                            }, 1800);
+                        } else{
+                            toastr.error(response.message, "Error !",{
+                                closeButton: true,
+                                progressBar: true,
+                                timeOut: 1500
+                            });
+                        }
+                    }
+                })
+            })
+        })
+    </script>
 </body>
 </html>
--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 function getConnection(): PDO{
     $host = "localhost";
-    $port = 3308;
+    $port = 3306;
     $database = "db_perpus";
     $username = "root";
-    $password = "";
+    $password = "devola3465";
 
     return new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
 }
